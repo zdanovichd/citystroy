dfbd2036ac8b930697230f6fcc695dc144cc7ccc
Edvard <pmkstudio77@gmail.com>
2025-09-19 17:49:23 +0300

    Внес новые страницы на сайт

---
+++ pages/o-kompanii.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<!-- HEAD -->
+<head>
+    <title>Пользовательское соглашение Ӏ Ситистрой</title>
+    <meta name="description" content="
+        Ознакомьтесь с правилами использования сайта и сервисов Ситистрой. Настоящее соглашение регулирует ваши права и обязанности при взаимодействии с нашими ресурсами.
+    ">
+    <?php
+    include('../components/head.php')
+    ?>
+</head>
+<!-- PRELOADER -->
+<?php
+include('../components/preloader.php')
+?>
+<!-- HEADER -->
+<?php
+include('../components/header.php')
+?>
+
+<body>
+<div class="container">
+    <h1>Политика конфиденциальности</h1>
+
+</div>
+
+<!-- FOTTER -->
+<?php
+include('../components/footer.php')
+?>
+
+</body>
+</html>

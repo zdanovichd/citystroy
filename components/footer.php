2bfccc0d8a83224788ad64b3e78fb43463643767
Edvard <pmkstudio77@gmail.com>
2025-09-23 15:59:59 +0300

    Изменил футер

--- components/footer.php
+++ components/footer.php
@@ -61,23 +61,6 @@
 	</div>
 <!-- menu__main-footer -->
 	<div class="main__menu-footer__con">
-        <div class="menu__main__contacts">
-            <div class="contacts-item">
-                <a href="tel:+79263634278">+7 (926) 363-42-78</a>
-                <a href="tel:+74951092996">+7 (495) 109-29-96</a>
-            </div>
-            <div class="contacts-item">
-                <a href="mailto:office@citystroy-restaurants.ru">office@citystroy-restaurants.ru</a>
-                <a href="mailto:csfitout@gmail.com">csfitout@gmail.com</a>
-            </div>
-            <div class="contacts-item">
-                <span>г. Москва, Береговой проезд, д. 7</span>
-            </div>
-            <div class="contacts-item">
-                <span>Пн-Сб: с 9:00 до 21:00</span>
-                <span>Вс: выходной</span>
-            </div>
-        </div>
         <ul  class="menu__main__container menu__main-footer">
             <li class="has-child">
                 <a class="menu__main__item" href="/o-kompanii">О компании</a>
@@ -93,6 +76,7 @@
                     <li><a class="menu__main__item" href="/blog">Блог</a></li>
                 </ul>
             </li>
+            <li><a class="menu__main__item" href="/proekty">Портфолио</a></li>
             <li class="has-child">
                 <a class="menu__main__item" href="/">Услуги</a>
                 <ul class="sub-menu">
@@ -110,35 +94,88 @@
                     <li><a class="menu__main__item" href="/">Открыть офис</a></li>
                 </ul>
             </li>
-            <li><a class="menu__main__item" href="/proekty">Портфолио</a></li>
-            <li><a class="menu__main__item" href="/contacts">Контакты</a></li>
-            <li><button class="button-menu button modal__btn">Связаться с нами</button></li>
         </ul>
-        <!-- Добавленные элементы -->
-        <div class="footer__legal">
-            <div class="footer__legal-links">
-                <a href="/политика-конфиденциальности" class="footer__legal-link">Политика конфиденциальности</a>
-                <a href="/пользовательское-соглашение" class="footer__legal-link">Пользовательское соглашение</a>
-                <a href="/реквизиты" class="footer__legal-link">Реквизиты</a>
+        <div class="menu__main__contacts">
+            <div class="contacts-item">
+                <p>
+                    Юридическое лицо: ООО «СитиСтрой» <br>
+                    ИНН: 7730212956 <br>
+                    ОГРН: 1167746840117 <br>
+                    КПП: 773001001 <br>
+                    Код организации по ОКПО: 04440612
+                </p>
             </div>
+            <div class="contacts-item">
+                <a class="menu__main__item" href="/contacts">Контакты</a>
+                <a href="tel:+79263634278">+7 (926) 363-42-78</a>
+                <a href="tel:+74951092996">+7 (495) 109-29-96</a>
+            </div>
+            <div class="contacts-item">
+                <a href="mailto:office@citystroy-restaurants.ru">office@citystroy-restaurants.ru</a>
+                <a href="mailto:csfitout@gmail.com">csfitout@gmail.com</a>
+            </div>
+            <button class="button-menu button modal__btn">Связаться с нами</button>
+            <div class="contacts-item">
+                <p>Мы в соцсетях</p>
+                <div>
+                    <a href="https://youtube.com/@citystroy?si=xDFF4QG_dSUSu89U" target="_blank" rel="noopener noreferrer" class="social-link">
+                        <svg class="social-icon" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+                            <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"/>
+                        </svg>
+                    </a>
 
-            <div class="footer__requisites">
-                <div class="requisites-item">
-                    <span>Юридическое лицо:</span>  ООО «СитиСтрой»
-                </div>
-                <div class="requisites-item">
-                    <span>Юридический адрес: </span> 121087, г. Москва, Береговой проезд, д.7, офис 3
-                </div>
-                <div class="requisites-item">
-                    <span>ИНН: 7730212956</span>
-                    <span>ОГРН: 1167746840117</span>
-                </div>
-                <div class="requisites-item">
-                    <span>КПП: 773001001</span>
-                    <span>Код организации по ОКПО: 04440612</span>
+                    <a href="https://t.me/travkov_mikhail" target="_blank" rel="noopener noreferrer" class="social-link">
+                        <svg class="social-icon" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+                            <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm5.894 8.221l-1.97 9.28c-.145.658-.537.818-1.084.508l-3-2.21-1.447 1.394c-.14.14-.26.26-.534.26l.213-3.053 5.56-5.022c.24-.213-.054-.334-.373-.121l-6.869 4.326-2.96-.924c-.64-.203-.658-.64.136-.954l11.566-4.458c.538-.196 1.006.128.832.941z"/>
+                        </svg>
+                    </a>
+
+                    <a href="https://dzen.ru/citystroyfit" target="_blank" rel="noopener noreferrer" class="social-link">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 166 167" fill="none">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M83.3295 166.23H82.6705C49.7081 166.23 30.436 162.915 17.0154 149.754C3.1854 135.664 0 116.522 0 83.6996V82.7709C0 49.9484 3.1854 30.6762 17.0154 16.7164C30.5658 3.56543 49.7081 0.240234 82.6605 0.240234H83.3295C116.152 0.240234 135.424 3.56543 148.985 16.7164C162.815 30.806 166 49.9484 166 82.7709V83.6996C166 116.532 162.805 135.804 148.985 149.754C135.424 162.905 116.292 166.23 83.3295 166.23ZM145.84 80.771C146.48 80.801 147 81.3308 147 81.9706H146.99V84.6498C146.99 85.2996 146.47 85.8294 145.83 85.8494C122.868 86.7191 108.902 89.6382 99.115 99.4252C89.338 109.202 86.4189 123.148 85.5392 146.071C85.5092 146.71 84.9794 147.23 84.3396 147.23H81.6504C81.0006 147.23 80.4708 146.71 80.4508 146.071C79.5711 123.148 76.652 109.202 66.875 99.4252C57.0981 89.6482 43.1225 86.7191 20.1596 85.8494C19.5198 85.8194 19 85.2896 19 84.6498V81.9706C19 81.3208 19.5198 80.791 20.1596 80.771C43.1225 79.9013 57.0881 76.9822 66.875 67.1952C76.672 57.3983 79.5911 43.4027 80.4608 20.3999C80.4908 19.7601 81.0206 19.2402 81.6604 19.2402H84.3396C84.9894 19.2402 85.5192 19.7601 85.5392 20.3999C86.4089 43.4127 89.328 57.3983 99.125 67.1952C108.902 76.9722 122.878 79.9013 145.84 80.771Z" fill="white"/>
+                        </svg>
+                    </a>
+
+                    <a href="https://ru.pinterest.com/travkovmai/" target="_blank" rel="noopener noreferrer" class="social-link">
+                        <svg height="24" width="24" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                             viewBox="0 0 23.189 23.189" xml:space="preserve">
+                            <g>
+                                <g>
+                                    <path style="fill:white;" d="M23.189,11.595c0,6.403-5.188,11.594-11.591,11.594c-1.641,0-3.204-0.34-4.62-0.959l0.023-11.574
+                                        c0,0-0.051-0.446,0.202-1.259c1.256-2.844,3.292-2.779,3.292-2.779c1.027-0.231,2.588,0.033,2.588,0.033
+                                        c3.156,0.832,3.491,3.721,3.491,3.721c0.165,0.635,0,1.494,0,1.494c-0.233,3.086-3.69,3.525-3.69,3.525l-2.223,0.065
+                                        c-0.931,0.664-0.765,1.36-0.765,1.36c-0.033,1.035,0.765,1.265,0.765,1.265c1.205,0.438,3.255-0.034,3.255-0.034
+                                        c4.849-0.729,5.448-6.015,5.448-6.015l0.123-1.178l-0.123-1.246c-1.096-4.552-5.846-5.781-5.846-5.781s-2.512-0.393-3.759,0
+                                        C6.025,4.81,4.783,7.79,4.402,9.166c-0.121,0.435-0.155,1.464-0.155,1.588c0,1.423,0,6.117,0,9.807
+                                        C1.655,18.436,0,15.209,0,11.594C0,5.191,5.19,0,11.596,0C18,0.001,23.189,5.192,23.189,11.595z"/>
+                                </g>
+                            </g>
+                        </svg>
+                    </a>
                 </div>
             </div>
+            <div class="contacts-item">
+                <span>г. Москва, Береговой проезд, д. 7</span>
+            </div>
+            <div class="contacts-item">
+                <p>
+                    Пн-Сб: с 9:00 до 21:00 <br>
+                    Вс: выходной
+                </p>
+            </div>
+            <div class="contacts-item">
+                <a href="/privacy-policy" class="footer__legal-link">Политика конфиденциальности</a>
+                <a href="/пользовательское-соглашение" class="footer__legal-link">Пользовательское соглашение</a>
+                <!-- Копирайт -->
+                <p>
+                    © 2024 CityStroy. Сайт носит исключительно информационный характер и не является публичной офертой,
+                    определяемой положениями Статьи 437 Гражданского кодекса Российской Федерации.
+                </p>
+            </div>
+            <!-- Социальные сети -->
+
         </div>
+
 	</div>
 </nav>
 <div class="C-mob-con">
